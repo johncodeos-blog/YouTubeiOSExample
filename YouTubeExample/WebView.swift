1e31a4311a8a25f37de66878aa59047d792928a1
John Codeos <johncodeos@gmail.com>
2023-10-10 13:05:09 +0300

    Remove UI delegate code because we don't use it

--- YouTubeExample/WebView.swift
+++ YouTubeExample/WebView.swift
@@ -9,39 +9,25 @@
 import UIKit
 import WebKit
 
-class WebView: UIViewController, WKUIDelegate {
-    
+class WebView: UIViewController {
     @IBOutlet weak var webPlayerView: UIView!
     
-    // Use a lazy var to initialize the WKWebView only when it's needed.
-    // This helps to reduce the initial load time of the view controller.
     lazy var webPlayer: WKWebView = {
         let webConfiguration = WKWebViewConfiguration()
         webConfiguration.allowsInlineMediaPlayback = true
-        
         let webView = WKWebView(frame: webPlayerView.bounds, configuration: webConfiguration)
-
-        webView.uiDelegate = self  // Set the UI delegate to handle any UI related tasks in the web view.
         return webView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupWebPlayer()
         loadVideoURL("https://www.youtube.com/embed/YE7VzlLtp-4?playsinline=1")
     }
     
     private func setupWebPlayer() {
         webPlayerView.addSubview(webPlayer)
-        webPlayer.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            webPlayer.topAnchor.constraint(equalTo: webPlayerView.topAnchor),
-            webPlayer.leadingAnchor.constraint(equalTo: webPlayerView.leadingAnchor),
-            webPlayer.trailingAnchor.constraint(equalTo: webPlayerView.trailingAnchor),
-            webPlayer.bottomAnchor.constraint(equalTo: webPlayerView.bottomAnchor)
-        ])
+        webPlayer.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
     
     private func loadVideoURL(_ urlString: String) {
@@ -49,9 +35,7 @@ class WebView: UIViewController, WKUIDelegate {
             print("Invalid URL")
             return
         }
-        
         let request = URLRequest(url: url)
         webPlayer.load(request)
     }
 }
-
